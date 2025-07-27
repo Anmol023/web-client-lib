cb4048782b78525c6f47ceee8e7b8da3f6c67b05
Anmol023 <anmolgupta023@gmail.com>
2025-07-27 17:58:41 +0530

    first commit

---
+++ src/main/kotlin/com/example/web_client_lib/WebClientLibApplication.kt
@@ -0,0 +1,11 @@
+package com.example.web_client_lib
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebClientLibApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebClientLibApplication>(*args)
+}
